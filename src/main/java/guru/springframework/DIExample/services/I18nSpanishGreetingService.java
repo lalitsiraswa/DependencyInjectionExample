667fa1def7abed4c270ecf3536829297dd2e99ef
lalitsiraswa <lalitsiraswa1999@gmail.com>
2022-09-07 23:05:38 +0530

    Default Profile

--- src/main/java/guru/springframework/DIExample/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/DIExample/services/I18nSpanishGreetingService.java
@@ -3,7 +3,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 // i18nService is a Name Qualifier
-@Profile("ES")
+//@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
